fbd0552159c460927fb0add0379148f531157b82
Mateeto2002 <mateoperezbaena@gmail.com>
2025-08-07 22:18:51 -0500

    creo funciones principales

--- app.js
+++ app.js
@@ -1,1 +1,49 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+
+let amigos = [];
+let aleatorio = Math.floor(Math.random()*amigos.length);
+let resultado = document.getElementById('resultado');
+let lista = document.getElementById("listaAmigos");
+let cajaTexto = document.getElementById("amigo");
+let nombre = cajaTexto.value;
+
+function agregarAmigo(){
+
+   
+
+    if (nombre == "" ) {
+        alert("Por favor escriba un nombre")
+
+    }else{
+
+        amigos.push(nombre);
+        console.log(amigos)
+    }
+    cajaTexto.value = "";
+    
+    agregarLista();
+   
+}
+
+function agregarLista() {
+    
+    lista.innerHTML = "";   
+
+    for(let i = 0; i < amigos.length; i++){
+
+        let li = document.createElement("li");
+        li.textContent = amigos[i];
+        lista.appendChild(li);
+
+    }
+
+}
+
+
+function sortearAmigo() {
+
+    lista.innerHTML = "";
+    resultado.innerHTML = "El nombre ganado es "+ amigos[aleatorio];
+    
+}
